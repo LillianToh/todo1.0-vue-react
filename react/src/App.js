c7c04d1583613eda8a8929d2b93d760873f9fd49
thainabbraz <32195098+thainabbraz@users.noreply.github.com>
2020-12-10 01:01:44 +0000

    Initial commit

---
+++ react/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h3>To Do List</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
